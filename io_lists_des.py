618ef7efc0f13cb111e4c41faa55bc40750098cc
ktaed <ktaed@nau.edu>
2018-11-30 09:40:51 -0700

    wrapped parser into a function

--- io_lists_des.py
+++ io_lists_des.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python
+def read_input(file_name):
+    f = open(file_name,'r')
+    lines = f.readlines()[46:430]
+    f.close()
 
-f = open('MH 3-8_QuantStudio_export.txt','r')
-lines = f.readlines()[46:430]
-f.close()
+    Samples = []
+    ct_mean = []
+    ct_sd = []
 
-Samples = []
-ct_mean = []
-ct_sd = []
+    for i in lines:
+        samples = []
+        cm = []
+        cs = []
+        samples.append(i.split('\t')[3])
+        for x in samples:
+            if x != 'empty':
+                Samples.append(x)
+        cm.append(i.split('\t')[9])
+        for x in cm:
+            if x != '':
+                ct_mean.append(x)
+        cs.append(i.split('\t')[10])
+        for x in cs:
+            if x != '':
+                ct_sd.append(x)
 
-for i in lines:
-    samples = []
-    cm = []
-    cs = []
-    samples.append(i.split('\t')[3])
-    for x in samples:
-        if x != 'empty':
-            Samples.append(x)
-    cm.append(i.split('\t')[9])
-    for x in cm:
-        if x != '':
-            ct_mean.append(x)
-    cs.append(i.split('\t')[10])
-    for x in cs:
-        if x != '':
-            ct_sd.append(x)
+    #del(lines)
 
-#del(lines)
+    Samples = sorted(set(Samples), key=lambda x: Samples.index(x))
+    ct_mean = sorted(set(ct_mean), key=lambda x: ct_mean.index(x))
+    ct_sd = sorted(set(ct_sd), key=lambda x: ct_sd.index(x))
 
-Samples = sorted(set(Samples), key=lambda x: Samples.index(x))
-ct_mean = sorted(set(ct_mean), key=lambda x: ct_mean.index(x))
-ct_sd = sorted(set(ct_sd), key=lambda x: ct_sd.index(x))
+    return Samples, ct_mean, ct_sd
