f1f4eecef5ebc1c933a34825ce2b995c4e417582
hkdailey <41974094+hkdailey@users.noreply.github.com>
2018-11-30 11:20:04 -0700

    Update Hackathon_2_plot.py

--- Hackathon_2_plot.py
+++ Hackathon_2_plot.py
@@ -4,15 +4,15 @@ import numpy as np
 import sklearn
 import matplotlib.pyplot as plt
 
-logConcentration = [1.30, 0.30, -0.70, -1.70, -2.70, -3.70] 
+logConcentration = [1.30, 0.30, -0.70, -1.70, -2.70, -3.70] #stays constant/unchanged
 avgCq = [8.718, 11.779, 15.414, 19.012, 22.247, 25.459] #read from file
 
 from sklearn.linear_model import LinearRegression
 #X = logConcentration
 #Y = avgCq
 
-X = np.asarray(logConcentration[0:5]).reshape(-1,1)
-Y = avgCq[0:5]
+X = np.asarray(logConcentration[0:5]).reshape(-1,1) #x values (remain constant/unchanged)
+Y = avgCq[0:5] #y values (will change)
 standardRegression = LinearRegression()
 
 ''''plt.scatter(X, Y)
